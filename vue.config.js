5db41dceff7fa260e2222987284dfd28d13ff9af
Ivan Alvaro <ivanprogamer43@gmail.com>
2022-09-21 18:48:06 +0200

    hola

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,10 @@
-const path = require("path");
-
-let previewBrowser = false
-let path2 = ''
-
-if (previewBrowser) {
-    path2 = './'
-} else {
-    path2 = '../dist'
+module.exports = {
+  publicPath: '',
+  outputDir: './dist',
+  assetsDir: './',
 }
 
-module.exports = { publicPath: path2, outputDir: '../dist', filenameHashing: false, productionSourceMap: false,
-configureWebpack: {
-    devtool: "source-map",
-    optimization: {
-      minimize: false
-    },
-    resolve: {
-      alias: {
-        // bind version of jquery-ui
-        "jquery-ui": "jquery-ui-dist/jquery-ui.js",
-        // bind to modules;
-        modules: path.join(__dirname, "node_modules")
-      }
-    }
-  }
-}
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
